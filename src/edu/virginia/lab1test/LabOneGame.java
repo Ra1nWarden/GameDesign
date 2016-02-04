5bbf182aa14c284f18a245aafec9736713093d81
Ra1nWarden <wzh19921016@gmail.com>
2016-02-03 20:54:54 -0500

    Initial commit from lab2

---
+++ src/edu/virginia/lab1test/LabOneGame.java
@@ -0,0 +1,78 @@
+package edu.virginia.lab1test;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
+
+import edu.virginia.engine.display.Game;
+import edu.virginia.engine.display.Sprite;
+
+/**
+ * Example game that utilizes our engine. We can create a simple prototype game with just a couple lines of code
+ * although, for now, it won't be a very fun game :)
+ * */
+public class LabOneGame extends Game{
+
+	/* Create a sprite object for our game. We'll use mario */
+	Sprite mario = new Sprite("Mario", "Mario.png");
+	Sprite mario2 = new Sprite("Mario2", "Mario.png");
+	
+	/**
+	 * Constructor. See constructor in Game.java for details on the parameters given
+	 * */
+	public LabOneGame() {
+		super("Lab One Test Game", 500, 300);
+	}
+	
+	/**
+	 * Engine will automatically call this update method once per frame and pass to us
+	 * the set of keys (as strings) that are currently being pressed down
+	 * */
+	@Override
+	public void update(ArrayList<String> pressedKeys){
+		super.update(pressedKeys);
+		
+		/* Make sure mario is not null. Sometimes Swing can auto cause an extra frame to go before everything is initialized */
+		if(mario != null) mario.update(pressedKeys);
+	}
+	
+	/**
+	 * Engine automatically invokes draw() every frame as well. If we want to make sure mario gets drawn to
+	 * the screen, we need to make sure to override this method and call mario's draw method.
+	 * */
+	@Override
+	public void draw(Graphics g){
+		super.draw(g);
+		
+		/* Same, just check for null in case a frame gets thrown in before Mario is initialized */
+		if(mario != null) {
+			mario.setxPosition(100);
+			mario.setyPosition(50);
+			mario.setPivotPoint(new Point(25, 25));
+			mario.setRotation(mario.getRotation() + 0.001);
+			mario.setScaleX(2);
+			mario.draw(g);
+		}
+		
+		if(mario2 != null) {
+			mario2.setVisible(false);
+			mario2.setxPosition(200);
+			mario2.setyPosition(100);
+			mario2.setAlpha(0.5f);
+			mario2.setPivotPoint(new Point(100, 50));
+			mario2.setRotation(Math.PI / 2);
+			mario2.setScaleY(0.5f);
+			mario2.draw(g);
+		}
+	}
+
+	/**
+	 * Quick main class that simply creates an instance of our game and starts the timer
+	 * that calls update() and draw() every frame
+	 * */
+	public static void main(String[] args) {
+		LabOneGame game = new LabOneGame();
+		game.start();
+
+	}
+}
